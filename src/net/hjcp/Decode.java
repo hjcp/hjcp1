44cbc422d7b271bef886e2229befc48dfaa18ad5
Roman Berla <rberla@ya.ru>
2014-08-14 14:44:04 +0300

    Update Decode.java
    I think, there were some auto mistakes

--- src/net/hjcp/Decode.java
+++ src/net/hjcp/Decode.java
@@ -4,8 +4,8 @@ public class Decode {
 
     public static void main(String[] args) {
 
-        Data myDate = new Data();
-        int[] myData = myDate.getData();
+        Data data = new Data();
+        int[] myData = data.getData();
 
         for (int i = 0; i < myData.length; i++) {
             System.out.printf("0x%X ", myData[i]);
