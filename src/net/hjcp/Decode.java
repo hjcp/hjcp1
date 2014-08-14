bd9ebb9bd9d2d77f1e340b3cc01e08f151e046c7
Hexlet Java Community Problems <hjcp@slavkin.info>
2014-08-14 18:14:52 +0400

    Revert "Один из вариантов решения."

--- src/net/hjcp/Decode.java
+++ src/net/hjcp/Decode.java
@@ -1,22 +1,15 @@
 package net.hjcp;
 
-import net.hjcp.Exceptions.ReadDataException;
-
 public class Decode {
 
-    public static void main(String[] args) throws ReadDataException{
+    public static void main(String[] args) {
 
         Data myDate = new Data();
         int[] myData = myDate.getData();
 
-        DataReader reader = new DataReader(myData);
-        try {
-            reader.ReadData();
-        }
-        catch (ReadDataException e) {
-            System.out.println(e);
+        for (int i = 0; i < myData.length; i++) {
+            System.out.printf("0x%X ", myData[i]);
         }
 
-        reader.PrintRecords();
     }
 }
