c9067482b2339a1ee9e285eed97ede006934dd6c
Roman Berla <rberla@ya.ru>
2014-08-14 18:46:11 +0300

    Revert "Update Decode.java"
    This reverts commit 44cbc422d7b271bef886e2229befc48dfaa18ad5.

--- src/net/hjcp/Decode.java
+++ src/net/hjcp/Decode.java
@@ -4,8 +4,8 @@ public class Decode {
 
     public static void main(String[] args) {
 
-        Data data = new Data();
-        int[] myData = data.getData();
+        Data myDate = new Data();
+        int[] myData = myDate.getData();
 
         for (int i = 0; i < myData.length; i++) {
             System.out.printf("0x%X ", myData[i]);
